ed97e3b704851fe36de9ca9255469e7632fbfd63
Andy Hall <andersonhall@gmail.com>
2025-03-05 21:04:12 -0600

    initial styles for posts

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,8 +1,24 @@
+import styles from "./posts.module.css";
+import upIcon from "../../assets/up.svg";
+import downIcon from "../../assets/down.svg";
+
 const Post = () => {
   return (
-    <div className="post">
-      <h2>Post Title</h2>
+    <div className={styles.post}>
+      <h2 className={styles.postTitle}>Post Title</h2>
       <img src="https://placehold.co/600x400" alt="placeholder" />
+      <div className={styles.postFooter}>
+        <div className={styles.postFooterLeft}>
+          <p>
+            <img src={upIcon} alt="up" />
+            <span>|</span>
+            <span>27</span>
+            <span>|</span>
+            <img src={downIcon} alt="down" />
+          </p>
+        </div>
+        <p className={styles.postFooterRight}>/Subreddit * 27 hr ago</p>
+      </div>
     </div>
   );
 };
